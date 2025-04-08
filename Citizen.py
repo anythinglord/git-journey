a8ea5a60c1040d5b341d440ec52c76e1ac159d10
anythinglord <sjsalcedoa@unal.edu.co>
2025-04-08 11:19:02 -0500

    simple simulation for citizen

--- Citizen.py
+++ Citizen.py
@@ -1,13 +1,29 @@
 from random import randint
 from util import get_name
+import time
 
 class Citizen:
     def __init__(self):
         self.name = get_name()
-        self.age = randint(20, 80)
+        self.age = 0
+        self.maxAge = randint(60, 80)
+        self.school = ''
 
     def getName(self):
         return self.name
     
     def getAge(self):
         return self.age
+    
+    def setSchool(self, school):
+        self.school = school
+    
+    def sickness(self, level):
+        return randint(0, 5 - level)
+
+    def simulate(self):
+        print('Max age: ', self.maxAge)
+        for age in range(self.maxAge):
+            if age == 5: print(self.name, 'entry in the school..')
+            if age == 16: print(self.name, 'exit of the school..')
+            time.sleep(0.05)
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from Town import Town
 
 print("Town v0.0.1")
 town = Town("xant")
+
 print('Town name: ', town.getName())
 print('Town population: ', town.getPopulation())
-for i in town.citizens:
-    print('Citizen name: ', i.getName())
+#for i in town.citizens:
+    #print('Citizen name: ', i.getName())
+print(town.citizens[0].simulate())
